8c1de1384835c42a8bc762fcfeaa3915cb258f80
Jose Martinez <jose.martinez@cit.hn>
2021-10-31 16:59:18 -0600

    +updating cors configuration

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
-	middleware "northwind-api/middleware"
+	"northwind-api/middleware"
 	usersRoutes "northwind-api/routes"
 
 	"github.com/gin-gonic/gin"
@@ -41,16 +42,25 @@ func main() {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		// First, we add the headers with need to enable CORS
+		// Make sure to adjust these headers to your needs
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "*")
+		c.Header("Access-Control-Allow-Headers", "*")
+		c.Header("Content-Type", "application/json")
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
+		// Second, we handle the OPTIONS problem
+		if c.Request.Method != "OPTIONS" {
+
+			c.Next()
+
+		} else {
+
+			// Everytime we receive an OPTIONS request,
+			// we just return an HTTP 200 Status Code
+			// Like this, Angular can now do the real
+			// request using any other method than OPTIONS
+			c.AbortWithStatus(http.StatusOK)
 		}
-
-		c.Next()
 	}
 }
